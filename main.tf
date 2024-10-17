457fb88c0857260cfcae0f165d2686a55ff5ea03
gremble0 <hermanstornes@gmail.com>
2024-10-17 09:33:49 +0200

    Copypasta

--- main.tf
+++ main.tf
@@ -1,76 +1,79 @@
 provider "aws" {
-   region = "eu-west-1"
+  region = "eu-west-1"
 }
 
-resource "aws_iam_role" "some_role" {
-   name = "some_role"
-   assume_role_policy = jsonencode({
-      Version = "2012-10-17",
-      Statement = [{
-         Action = "sts:AssumeRole",
-         Effect = "Allow",
-         Principal = {
-         Service = "lambda.amazonaws.com"
-         }
-      }]
-   })
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "hermanstornes@gmail.com"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
 }
 
-resource "aws_lambda_function" "backend" {
-   filename         = "todo-backend/build/libs/TodoApp-0.0.2-shadow.jar"
-   function_name    = "TodoAppHermanStornesTF"
-   role             = aws_iam_role.some_role.arn
-   handler          = "com.booleanuk.StreamLambdaHandler::handleRequest"
-   runtime          = "java21"
-   source_code_hash = filebase64sha256("todo-backend/build/libs/TodoApp-0.0.2-shadow.jar")
-   memory_size      = 1024
-   timeout          = 10
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-resource "aws_api_gateway_rest_api" "api" {
-   name = "TodoAppHermanStornesTF"
-}
-
-resource "aws_api_gateway_resource" "resource" {
-   rest_api_id = aws_api_gateway_rest_api.api.id
-   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-   path_part   = "todos"
-}
-
-resource "aws_api_gateway_method" "method" {
-   rest_api_id   = aws_api_gateway_rest_api.api.id
-   resource_id   = aws_api_gateway_resource.resource.id
-   http_method   = "GET"
-   authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "integration" {
-   rest_api_id = aws_api_gateway_rest_api.api.id
-   resource_id = aws_api_gateway_resource.resource.id
-   http_method = aws_api_gateway_method.method.http_method
-   type        = "AWS_PROXY"
-   integration_http_method = "POST"
-   uri         = aws_lambda_function.backend.invoke_arn
-}
-
-resource "aws_api_gateway_deployment" "deployment" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  stage_name  = "todos"
-
-  depends_on = [aws_api_gateway_integration.integration]
-}
-
-resource "aws_lambda_permission" "apigw_lambda" {
+resource "aws_lambda_permission" "api_gateway_invoke" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.backend.function_name
   principal     = "apigateway.amazonaws.com"
-
-  # The /*/* portion grants access from any method on any resource
-  # within the API Gateway "REST API".
-  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
+  source_arn    = "arn:aws:execute-api:eu-west-1:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api.id}/*/*"
 }
 
-output "base_url" {
+resource "aws_lambda_function" "backend" {
+  s3_bucket        = "herman-stornes-bucket-terraform"
+  s3_key           = "backend.zip"
+  function_name    = "herman-stornes-terraform"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "com.booleanuk.StreamLambdaHandler::handleRequest"
+  runtime          = "java21"
+  memory_size      = 512
+  timeout          = 120
+}
+
+resource "aws_api_gateway_rest_api" "api" {
+  name = "herman-stornes-api"
+}
+
+resource "aws_api_gateway_resource" "resource" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "todos"
+}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.resource.id
+  http_method             = aws_api_gateway_method.method.http_method
+  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri                     = aws_lambda_function.backend.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on = [aws_api_gateway_integration.integration]
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = "prod"
+}
+
+output "api_url" {
   value = aws_api_gateway_deployment.deployment.invoke_url
 }
